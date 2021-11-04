a2cfbe8fe88bbd090091ad6ffa5f41a46a5935f6
Raul Sauco <sauco.raul@gmail.com>
2021-11-04 09:43:12 +0100

    Implemented authentication feature remote data source

---
+++ lib/core/environment/environment_config.dart
@@ -0,0 +1,19 @@
+/// Environment configuration file.
+///
+/// Environments have sane defaults that can be configured passing command line
+/// arguments, for example:
+///
+/// ```flutter run --dart-define=APP_NAME=DevelopmentApp```
+///
+/// ```flutter test --dart-define=API_URL=api.example.com```
+class EnvironmentConfig {
+  static const apiUrl = String.fromEnvironment(
+    'API_URL',
+    defaultValue: 'apiv1.wikiclimb.org',
+  );
+
+  static const appName = String.fromEnvironment(
+    'APP_NAME',
+    defaultValue: 'WikiClimb',
+  );
+}
--- lib/features/authentication/data/datasources/authentication_remote_data_source.dart
+++ lib/features/authentication/data/datasources/authentication_remote_data_source.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:wikiclimb_flutter_frontend/core/environment/environment_config.dart';
+import 'package:wikiclimb_flutter_frontend/core/error/exception.dart';
+
 import '../models/authentication_data_model.dart';
 
 abstract class AuthenticationRemoteDataSource {
@@ -6,4 +12,30 @@ abstract class AuthenticationRemoteDataSource {
   /// Throws a [ServerException] for all error codes.
   Future<AuthenticationDataModel> login(
       {required String username, required String password});
+}
+
+class AuthenticationRemoteDataSourceImpl
+    implements AuthenticationRemoteDataSource {
+  AuthenticationRemoteDataSourceImpl({required this.client});
+
+  final http.Client client;
+
+  @override
+  Future<AuthenticationDataModel> login(
+      {required String username, required String password}) async {
+    final url = Uri.https(
+      EnvironmentConfig.apiUrl,
+      'login',
+    );
+    final response = await client.post(
+      url,
+      body: {'username': username, 'password': password},
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return AuthenticationDataModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
